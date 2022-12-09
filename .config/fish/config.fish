fbc9d2e0bad599a0ca276ed804fb8788a217b738
James Martinez <jamescmartinez@gmail.com>
2022-12-09 12:50:32 -0600

    Remove unused aliases and functions from config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,27 +2,7 @@
 set fish_greeting
 
 # Aliases
-alias erbl='erblint --lint-all --autocorrect'
-alias flushdns='sudo killall -HUP mDNSResponder'
 alias g='git'
-alias http='python -m http.server'
-alias rdbm='rake db:migrate'
-alias rmdd='rm -rf ~/Library/Developer/Xcode/DerivedData/'
-alias rubo='rubocop -A'
-alias tf='terraform'
-alias v='vim'
-alias vi='vim'
-
-# Functions
-function deploy
-  set branch (git symbolic-ref --short -q HEAD)
-  git push $argv $branch:master
-end
-
-function kittyup
-  # https://sw.kovidgoyal.net/kitty/binary
-  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-end
 
 # asdf
 source (brew --prefix asdf)'/asdf.fish'
