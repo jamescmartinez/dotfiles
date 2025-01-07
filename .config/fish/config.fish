9eb95d28122be083dad404b7087e66ac7b720ef5
James Martinez <jamescmartinez@gmail.com>
2025-01-07 12:10:11 -0600

    Add `list_git_repos` function to Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,3 +22,16 @@ source (brew --prefix)'/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.f
 # Editor Config
 set -x VISUAL code --wait
 set -x EDITOR code --wait
+
+# List all git repositories in the current directory ordered by their most
+# recent commit.
+function list_git_repos
+    for dir in */
+        if test -d "$dir/.git"
+            set commit_date (git -C "$dir" log -1 --format="%at" 2>/dev/null)
+            if test -n "$commit_date"
+                echo (date -r $commit_date '+%Y-%m-%d %H:%M:%S')" $dir"
+            end
+        end
+    end | sort -r | head -n 10
+end
