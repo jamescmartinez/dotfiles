30a96350e88b2bda85bd67aa8042d2f9f33c23c6
James Martinez <jamescmartinez@gmail.com>
2025-07-29 17:21:18 -0500

    Add `dns_flush` to flush DNS

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ alias assume="source (brew --prefix)/bin/assume.fish"
 alias dns_cloudflare="networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001" # Use Cloudflare DNS servers
 alias dns_default='networksetup -setdnsservers Wi-Fi Empty' # Remove custom DNS servers
 alias dns_list='networksetup -getdnsservers Wi-Fi' # List custom DNS servers
+alias dns_flush='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder' # Flush DNS cache
 alias g='git'
 alias glci='golangci-lint'
 alias kittyup='curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin' # Update Kitty
