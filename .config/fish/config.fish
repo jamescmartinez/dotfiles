2b01c0249f4c55d71dd14c4c34ba7233bce150a9
James Martinez <jamescmartinez@gmail.com>
2025-07-31 16:46:15 -0500

    Remove `gcloud`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,9 +18,6 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-# gcloud
-source (brew --prefix)'/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc'
-
 # Editor Config
 set -x VISUAL code --wait
 set -x EDITOR code --wait
